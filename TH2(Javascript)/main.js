28249604d704337fd2c0c55f7e9ac46863e88d10
vinhnasa <20200122@eaut.edu.vn>
2023-02-28 14:16:29 +0700

    20

---
+++ TH2(Javascript)/main.js
@@ -0,0 +1,9 @@
+function findMaxNumber(num1, num2){
+    if(num1>num2){
+        return console.log("so lon nhat la "+num1);
+    }else{
+        return console.log("so lon nhat la "+num2);
+    }
+};
+
+findMaxNumber(10000,2192)
