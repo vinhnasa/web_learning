948f75982162f8e226ac946bdbf85275549120c6
vinhnasa <20200122@eaut.edu.vn>
2023-03-31 15:13:15 +0700

    38

---
+++ TH1(reactjs)/function.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function console() {
+    var isUserLoggedIn =  true;
+    if(isUserLoggedIn =  true){
+        console.log('Hello User')
+    }else{
+        console.log('Login Now')
+    }
+}
