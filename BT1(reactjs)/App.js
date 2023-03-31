8033e4b6907435b4adc19534abce532355d234fe
vinhnasa <20200122@eaut.edu.vn>
2023-03-31 15:33:42 +0700

    39

--- BT1(reactjs)/App.js
+++ BT1(reactjs)/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import console from './function';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <console/>
   );
 }
 
